47b2b73236b9b570a655708cbc0c96000d77cb50
BimaMalabonga <b.malabonga@det.nl>
2025-11-04 20:03:53 +0100

    Setup project

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from src.main import main
 
 if __name__ == "__main__":
     # Print package name and version, as defined in pyproject.toml
-    distribution_metadata = importlib.metadata.metadata("poetrytemplate")
+    distribution_metadata = importlib.metadata.metadata("ci_testing")
     print(f">> {distribution_metadata['Name']} v.{distribution_metadata['Version']} <<")
 
     # Load environment variables from local file '.env' (if file exists)
