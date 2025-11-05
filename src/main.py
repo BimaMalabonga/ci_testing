984dd90c97942b943229101d5645b1b949cd7482
BimaMalabonga <b.malabonga@det.nl>
2025-11-05 03:42:25 +0100

    wip

--- src/main.py
+++ src/main.py
@@ -6,5 +6,5 @@ import pandas as pd
 def main():
     outputs_dir = Path.cwd().joinpath("Outputs")
     outputs_dir.mkdir(parents=True, exist_ok=True)
-    df = pd.DataFrame(data=[[1, 2], [3, 4]])
+    df = pd.DataFrame(data=[[2, 2], [3, 4]])
     df.to_csv(outputs_dir.joinpath("df.csv"))
